c099075c9282083f022bb8a37ab341f5697953ae
LenaKolpakova <lena.kolpakova07@mail.ru>
2026-02-25 14:10:49 +0300

    lala

--- src/Cointer.jsx
+++ src/Cointer.jsx
@@ -4,12 +4,15 @@ export default function Cointer(){
     const [x, setx] = useState(0);
     const minus = () =>{
         setx(x - 1);}
-        return(
-    <>
-    <h1>{x}</h1>
-    <button onClick={minus}>-</button></>
-    
-)
-};
-
+    const plus = () =>{
+        setx(x + 1);
+    }
+    return (
+        <>
+            <h1>{x}</h1>
+            <button onClick={minus}>-</button>
+            <button onClick={plus}>+</button>
+        </>
+    );
+}
 
